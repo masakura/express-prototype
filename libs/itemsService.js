4a3d951f933d6b73219f8f3c85ca5d86b0de19ae
Tomo Masakura <tomo.masakura@gmail.com>
2016-06-08 22:49:21 +0900

    ショッピングサイトを作成

--- libs/itemsService.js
+++ libs/itemsService.js
@@ -31,6 +31,7 @@ class ItemsService {
       params += `&query=${query}`;
     }
 
+
     // kintone API の URL を作る。
     // https://cybozudev.zendesk.com/hc/ja/articles/202331474-%E3%83%AC%E3%82%B3%E3%83%BC%E3%83%89%E3%81%AE%E5%8F%96%E5%BE%97-GET-#step2
     const uri = `${kintoneApp.base}records.json?app=${kintoneApp.id}${params}`;
